6af127c243ffcd239ada90b5a9719e17312fb810
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-07-19 12:39:24 +0530

    [HEMANT]Add: Describe the data of given data

---
+++ Reflection_Demo/CustomerClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_Demo
+{
+    public class CustomerClass
+    {
+        public int a, b;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public CustomerClass()
+        {
+            Id = 1;
+            Name = "HEMANT";
+        }
+        public CustomerClass(int id)
+        {
+            id = id;
+
+        }
+        public void Display()
+        {
+
+        }
+        public void Update(int d)
+        {
+
+        }
+
+    }
+}
